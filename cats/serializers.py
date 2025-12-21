47f0f3cc24f2c44d21afe96d743821b74c8733e0
Tessa Gerasimova <tessagerasimova@gmail.com>
2025-12-21 17:29:03 +0200

    serializers (+ your own types)

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,13 +1,52 @@
 from rest_framework import serializers
+import datetime as dt
+import webcolors
+from .models import Cat, Owner, Achievement, AchievementCat, CHOICES
 
-from .models import Cat, Owner, Achievement, AchievementCat
+class OwnerSerializer(serializers.ModelSerializer):
+    cats = serializers.StringRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Owner
+        fields = ('first_name', 'last_name', 'cats')
+
+class AchievementSerializer(serializers.ModelSerializer):
+    achievement_name = serializers.CharField(source='name')
+
+    class Meta:
+        model = Achievement
+        fields = ('id', 'achievement_name')
+
+class Hex2NameColor(serializers.Field):
+    # При чтении данных ничего не меняем - просто возвращаем как есть
+    def to_representation(self, value):
+        return value
+    # При записи код цвета конвертируется в его название
+    def to_internal_value(self, data):
+        # Доверяй, но проверяй
+        try:
+            # Если имя цвета существует, то конвертируем код в название
+            data = webcolors.hex_to_name(data)
+        except ValueError:
+            # Иначе возвращаем ошибку
+            raise serializers.ValidationError('Для этого цвета нет имени')
+        # Возвращаем данные в новом формате
+        return data
+
 
 class CatSerializer(serializers.ModelSerializer):
     achievements = AchievementSerializer(many=True, required=False)
+    age = serializers.SerializerMethodField()
+    # Теперь поле примет только значение, упомянутое в списке CHOICES
+    color = serializers.ChoiceField(choices=CHOICES)
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements',
+                  'age')
+
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year 
 
     def create(self, validated_data):
         # Если в исходном запросе не было поля achievements
@@ -29,16 +68,3 @@ class CatSerializer(serializers.ModelSerializer):
             AchievementCat.objects.create(
                 achievement=current_achievement, cat=cat)
         return cat
-
-class OwnerSerializer(serializers.ModelSerializer):
-    cats = serializers.StringRelatedField(many=True, read_only=True)
-
-    class Meta:
-        model = Owner
-        fields = ('first_name', 'last_name', 'cats')
-
-class AchievementSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Achievement
-        fields = ('id', 'name')
